8d25640b22a2a23c858d13697c02b427bef6223c
Karim AIT BOUADDI <karimaitbouaddi@etu-digitalschool.paris>
2026-02-28 06:52:51 +0100

    Initial Commit

---
+++ Velox/VeloxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloxApp.swift
+//  Velox
+//
+//  Created by Karim AIT BOUADDI on 28/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct VeloxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VeloxTests/VeloxTests.swift
@@ -0,0 +1,17 @@
+//
+//  VeloxTests.swift
+//  VeloxTests
+//
+//  Created by Karim AIT BOUADDI on 28/02/2026.
+//
+
+import Testing
+@testable import Velox
+
+struct VeloxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
